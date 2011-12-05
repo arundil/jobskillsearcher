f747087756ffe60828b401698008cd937da8b6d5
marcetsii <marcetsii@a1d12387-2ab6-a87c-17ce-40e8bb2a5b32>
2011-12-05 13:48:13 +0000

    gran avance

---
+++ php/jss/tends.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>--JobSkillSearcher--</title>
+
+<script language="JavaScript" src="media/javascript/jstemplate1.js"
+	type="text/javascript"></script>
+<link rel="stylesheet" type="text/css" href="media/css/estilos.css">
+</head>
+
+<body>
+	<div id=container>
+		<div id="header">
+			<div id=logo>
+				<h1>JobSkillSearcher</h1>
+				<h3>Easy way to find skills you need for your future job</h3>
+			</div>
+			<div id=searcher>
+				<form action="#" method="GET">
+					<label for="s1">Search!</label> <input type="text" name="q"
+						id="s1" />
+					<button type="submit">GO!</button>
+				</form>
+			</div>
+ 		<div class="nofloat"></div>
+		</div>	
+		
+		<div id=content>	
+			<div id=search>
+				<div id= "menu_paginado">
+					<a href="results.php">Look word results</a>
+				</div>
+			</div>
+			<div id=contenidoprincipal>
+				MAIN CONTENT</br>
+			</div>
+		</div>
+			<div id="footer">
+				<div id=options>
+					<a href="">Contact us</a> <a href="">Help</a>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
